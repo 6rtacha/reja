11d1f9310b53f66bc6260b5b3771074cc8f09d1d
kevin <ahadjon1999bb@gmail.com>
2024-08-14 04:07:32 +0900

    dars 19

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 console.log('Web serverni boshlash');
 const express = require("express");
 const app = express();
+const res = require("express/lib/response");
 const http = require("http");
 
 // 1 Kirish code
@@ -14,12 +15,15 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style='background: red'>HELLO WORLD! by Kevin</h1>`);
-});
-app.get("/gift", function(req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-});
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({ test: "succes" });
+    
+})
+
+ app.get("/", function(req, res) {
+    res.render("harid");
+ }); 
 
 const server = http.createServer(app);
 let PORT = 3000;
