8087a937c1f524df982669bb4b87a19b77793f8d
kevin <ahadjon1999bb@gmail.com>
2024-08-15 15:28:52 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const express = require("express");
 const app = express();
 const res = require("express/lib/response");
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+        
+    }else {
+
+        user = JSON.parse(data);
+    }
+});
 // 1 Kirish code
 app.use(express.static("public")); // har qanday browserd kirib kelayotgan requestlar uchun public folder ochiq
 app.use(express.json());  // kirib kelayot json formatidagi data ni object holatiga ogirib beradi
@@ -25,6 +36,9 @@ app.post("/create-item", (req, res) => {
     res.render("harid");
  }); 
 
+  app.get('/author', (req, res) => {
+    res.render("author", { user: user });
+  })
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
