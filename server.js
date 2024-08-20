dd291a6860402fd71cfcdf1f792c702404c3faf4
kevin <ahadjon1999bb@gmail.com>
2024-08-21 00:15:56 +0900

    fix: callback hamda async functions

--- server.js
+++ server.js
@@ -11,14 +11,13 @@ fs.readFile("database/user.json", "utf-8", (err, data) => {
         console.log("ERROR:", err);
         
     }else {
-
         user = JSON.parse(data);
     }
 });
 // 1 Kirish code
 app.use(express.static("public")); // har qanday browserd kirib kelayotgan requestlar uchun public folder ochiq
 app.use(express.json());  // kirib kelayot json formatidagi data ni object holatiga ogirib beradi
-app.use(express.urlencoded({extended: true})) // form dan qabul qilingan datalarni express qabul qiladi serverga kiritadi
+app.use(express.urlencoded({extended: true})) // form dan qabul qilingan datalarni express qabul qiladi serverga kiritadi, (nested objectlarni)
 
 // 2: Session code
 // 3 Views code
@@ -26,7 +25,7 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing
-app.post("/create-item", (req, res) => {
+app.post("/create-item", (req, res) => { // head body url
     console.log(req);
     res.json({ test: "succes" });
     
--- train.js
+++ train.js
@@ -1,5 +1,7 @@
 console.log("Jack Ma maslahatlari");
 
+// nodejs single thread - bir xonali degani
+//synchronus function - immedietly ishga tushuvchi va javobni tezda talab qiliuvchi functionlar
 const list = [
     "yaxshi talaba bo'ling", //0-20
     "to'g'ri boshliq tanlang va ko'proq xato qiling", //20-30
@@ -9,8 +11,74 @@ const list = [
     "endi dam oling, foydasi yo'q" //60
 ];
 
+// Async Function (sync functionlar hammasi ishga tushgandan keyin nodejs async functionlar bilan ishalay boshlaydi, 
+// shuning uchun single threadni umuman band qilmaydi, event loop orqali thread pool ga tashlayveradi)
+
 function maslahatBering (a, callBack) {
-    if(typeof a !== 'number')callBack("insert a number", null);// callBack funksiyada birinchi parametr error boladi, ikkinchisi biz qaytarmoqchi bolgan data
+    if(typeof a !== 'number')callBack("insert a number", null);// callBack funksiyada birinchi parametr error boladi, ikkinchisi biz qaytarmoqchi bolgan data boladi
+    else if(a <= 20) callBack(null, list[0]);
+    else if(a > 20 && a <= 30) setTimeout(function(){callBack(null, list[1])}, 5000);
+    else if(a > 30 && a <= 40) callBack(null, list[2]);
+    else if(a > 40 && a <= 50) callBack(null, list[3]);
+    else if(a > 50 && a <= 60) callBack(null, list[4]);
+    else{
+        setInterval(function(){
+            callBack(null, list[5]);
+        }, 3000)
+        
+    }
+    
+};
+console.log('passed here 0');
+
+maslahatBering(70, (err, data) => {  
+    if(err) console.log('ERROR:', err);
+    else{
+        console.log("javob:", data);
+        
+    }
+});
+console.log('passed here 1');
+// async function maslahatBering (a) {
+//     if(typeof a !== 'number') throw new Error("insert a number", null);
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else{
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[5]);
+//             }, 5000);
+//         });
+//     }
+// };
+// then/catch
+// console.log('passed here 0');
+// maslahatBering(35)
+//     .then((data) => {
+//         console.log("javob: ", data);
+//     })
+//     .catch((err) => {
+//         console.log("ERROR: ", err);
+//     })
+// console.log('passed here 1');
+
+// aysnc / await
+// async function run() {
+//     let javob = await maslahatBering(20); //brirnchisi tugallanmasdan ikkinchisiga otmaydi
+//     console.log(javob);
+//     javob = await maslahatBering(78);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
+
+//Callback Function
+/*function maslahatBering (a, callBack) {
+    if(typeof a !== 'number')callBack("insert a number", null);// callBack funksiyada birinchi parametr error boladi, ikkinchisi biz qaytarmoqchi bolgan data boladi
     else if(a <= 20) callBack(null, list[0]);
     else if(a > 20 && a <= 30) setTimeout(function(){callBack(null, list[1])}, 5000);
     else if(a > 30 && a <= 40) callBack(null, list[2]);
@@ -33,4 +101,4 @@ maslahatBering(25, (err, data) => {  //parametr sifatida function ishlayapti
         
     }
 });
-console.log('passed here 1');
+console.log('passed here 1');*/
