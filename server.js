c76ecbd3585b9ff1945f8974e188d24b471fc6a7
kevin <ahadjon1999bb@gmail.com>
2024-08-23 07:13:59 +0900

    feat: mongodb ulanish logic

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-console.log('Web serverni boshlash');
-const express = require("express");
-const app = express();
-const res = require("express/lib/response");
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
+let db;
+const connectionString = "mongodb+srv://Akhadjon:MongoDb.6rtacha@cluster0.xn86n.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDb");
+    else{
+        console.log("MongoDb connection succeed");
+        module.exports = client;
         
-    }else {
-        user = JSON.parse(data);
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function() {
+            console.log(`The server is running on port ${PORT} http://localhost:3000`);
+        });
     }
-});
-// 1 Kirish code
-app.use(express.static("public")); // har qanday browserd kirib kelayotgan requestlar uchun public folder ochiq
-app.use(express.json());  // kirib kelayot json formatidagi data ni object holatiga ogirib beradi
-app.use(express.urlencoded({extended: true})) // form dan qabul qilingan datalarni express qabul qiladi serverga kiritadi, (nested objectlarni)
-
-// 2: Session code
-// 3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing
-app.post("/create-item", (req, res) => { // head body url
-    console.log(req);
-    res.json({ test: "succes" });
     
-})
-
- app.get("/", function(req, res) {
-    res.render("reja");
- }); 
-
-  app.get('/author', (req, res) => {
-    res.render("author", { user: user });
-  })
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running on port ${PORT} http://localhost:3000`);
 });
+
