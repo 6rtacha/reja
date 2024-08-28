b858b05d7a8d4e1babbe88f5b45e1a2ed0d70cd8
kevin <ahadjon1999bb@gmail.com>
2024-08-28 22:41:24 +0900

    fix: port = 7008

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongodb.connect(connectionString, {
         
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = 3000;
+        let PORT = 7008;
         server.listen(PORT, function() {
             console.log(`The server is running on port ${PORT} http://localhost:3000`);
         });
