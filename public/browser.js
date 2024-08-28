8d22549d7f338652b2bcbc336728c8f4ddc76033
kevin <ahadjon1999bb@gmail.com>
2024-08-28 09:09:08 +0900

    feat: Reja loyihamizning yakuniy bosqich logic

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,4 @@
+
 console.log('Fronted js ishga tushdi');
 
 function itemTemplate(item) {
@@ -49,6 +50,30 @@ document.addEventListener("click", function(e) {
     }
     // edit oper
     if(e.target.classList.contains("edit-me")) {
-        alert("Siz edit tugmasini bosdingiz!")
+        let userInput = prompt(
+            "O'zgartirish kiriting",
+            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML );
+        if(userInput){
+            axios
+                .post("/edit-item", {
+                    id: e.target.getAttribute("data-id"),
+                    new_input: userInput,
+                })
+                .then((response) => {
+                    console.log(response);
+                    e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+                })
+                .catch((err) => {
+                    console.log("Iltimos qaytadan harakat qiiling!");  
+                });
+            
+        }
     }
-})
+});
+
+document.getElementById("clean-all").addEventListener("click", function() {
+    axios.post("/delete-all", { delete_all: true}).then((response)  => {
+        alert(response.data);
+        document.location.reload();
+    });
+});
